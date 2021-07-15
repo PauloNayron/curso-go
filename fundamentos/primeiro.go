6d0eee1ca73d12b2e609a399a1b038a37c5e0860
PauloNayron <paulonayron.s@hotmail.com>
2021-07-15 10:40:31 -0300

    feat: hello world

---
+++ fundamentos/primeiro.go
@@ -0,0 +1,13 @@
+// Programas executáveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+import "fmt"
+
+// A porta de entrada de uma programa Go é a função main
+func main() {
+	fmt.Println("Hello World!")
+}
